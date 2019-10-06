1d218216db6e21987826c65ee2ff90dec6b532b9
Josemar <josemar.rincon@gmail.com>
2019-10-06 09:16:45 -0300

    initial commit

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+});
+
+export default api;
