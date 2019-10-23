8c909d8f1a79c1b4341d819f92ed1b2d8b09b15d
Josemar <josemar.rincon@gmail.com>
2019-10-23 00:00:49 -0300

    teset heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 console.log(process.env.NODE_ENV);
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL
